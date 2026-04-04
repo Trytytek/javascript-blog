55ba39f6ba6937af222e29ee0d5732e2a78e21e3
Hugo <pigtek@gmail.com>
2026-04-04 16:50:33 +0200

    Add remove class

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
-  /* remove class 'active' from all article links  */
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all articles */
+        for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
-  /* get 'href' attribute from the clicked link */
+    /* add class 'active' to the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-  /* add class 'active' to the correct article */
+        for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
